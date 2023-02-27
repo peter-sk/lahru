16197d1fe0e4f07e5de9c6c1c25ca4661a707981
Prof. Peter Schneider-Kamp <jps@ordbogen.com>
2023-02-27 22:18:58 +0100

    resume option multiple datasets
    Former-commit-id: 16c2d6709d5044dba97581ad46bcd28b2920574b

--- train.py
+++ train.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
 from sys import argv
-from datasets import load_dataset, DatasetDict
+from datasets import concatenate_datasets, load_dataset, DatasetDict
 from transformers import GPT2TokenizerFast,GPT2LMHeadModel, DataCollatorForLanguageModeling, TrainingArguments, Trainer
 model_name = 'gpt2-da'
 tokenizer = GPT2TokenizerFast.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 collator = DataCollatorForLanguageModeling(tokenizer=tokenizer,mlm=False)
-raw_dataset = load_dataset(f"data/{argv[1]}",data_files=f"{argv[1]}.jsonl.bz2").rename_column('text','input_ids')
-#reduced_dataset = raw_dataset['train'].train_test_split(0.99,shuffle=False,seed=42)
-train_testvalid = raw_dataset['train'].train_test_split(0.1,shuffle=False,seed=42)
-test_valid = train_testvalid['test'].train_test_split(0.5,shuffle=False,seed=42)
+datasets = []
+args = argv[1:]
+resume = False
+if args[0] == "--resume":
+    resume = True
+    args = args[1:]
+for arg in args:
+    raw_dataset = load_dataset(f"data/{arg}",data_files=f"{arg}.jsonl.bz2").rename_column('text','input_ids')
+    #reduced_dataset = raw_dataset['train'].train_test_split(0.99,shuffle=False,seed=42)
+    train_testvalid = raw_dataset['train'].train_test_split(0.1,shuffle=False,seed=42)
+    test_valid = train_testvalid['test'].train_test_split(0.5,shuffle=False,seed=42)
+    dataset = DatasetDict({
+        'train': train_testvalid['train'],
+        'test': test_valid['train'],
+        'valid': test_valid['test']
+    })
+    print(arg,dataset)
+    datasets.append(dataset)
 dataset = DatasetDict({
-    'train': train_testvalid['train'],
-    'test': test_valid['train'],
-    'valid': test_valid['test']
+    'train': concatenate_datasets([dataset['train'] for dataset in datasets]),
+    'test': concatenate_datasets([dataset['test'] for dataset in datasets]),
+    'valid': concatenate_datasets([dataset['valid'] for dataset in datasets])
 })
-print(dataset)
+del datasets
+print('MERGED',dataset)
 num_embeddings = model.transformer.wte.num_embeddings
 #tokenized_dataset = dataset.map(lambda row: tokenizer(row['input_ids'])).filter(lambda row: len(row) < 512 and all((x < num_embeddings for x in row['input_ids'])))
 tokenized_dataset = dataset.map(lambda row: tokenizer(row['input_ids'])).filter(lambda row: len(row['input_ids']) < 1024 and all((x < num_embeddings for x in row['input_ids'])))
@@ -44,5 +59,5 @@ trainer = Trainer(
     train_dataset=tokenized_dataset['train'],
     eval_dataset=tokenized_dataset['test']
 )
-trainer.train()
+trainer.train(resume_from_checkpoint=resume)
 trainer.save_model()
